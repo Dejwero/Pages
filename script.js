ae188a5c819d1d17cf727c1b619d6de1c0fbf770
Dejwero <101600531+Dejwero@users.noreply.github.com>
2023-06-08 22:48:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-// Znajdź wszystkie linki nawigacyjne
-const navLinks = document.querySelectorAll("nav a");
-
-// Dodaj obsługę zdarzenia click dla każdego linka nawigacyjnego
-navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault(); // Zatrzymaj domyślne zachowanie kliknięcia linka
-    const targetId = link.getAttribute("href"); // Pobierz ID docelowej sekcji z atrybutu href
-    const targetSection = document.querySelector(targetId); // Znajdź docelową sekcję na podstawie ID
-
-    // Przewiń do docelowej sekcji z płynnością
-    targetSection.scrollIntoView({ behavior: "smooth" });
-  });
-});
-
 function toggleDescription(sectionId) {
   const section = document.getElementById(sectionId);
   section.classList.toggle("section-expanded");
   const planetImage = section.querySelector(".planet-image");
   const planetModel = section.querySelector(".planet-model");
+  const moreDescription = section.querySelectorAll(".more-description");
 
+  const readMoreButton = section.querySelector(".read-more-button");
   if (section.classList.contains("section-expanded")) {
     planetImage.style.display = "none";
     planetModel.style.display = "block";
+    moreDescription.forEach((description) => {
+      description.style.display = "block";
+    });
+    document.querySelector(".container").classList.add("no-scroll");
+    readMoreButton.textContent = "Powrót";
   } else {
     planetImage.style.display = "block";
     planetModel.style.display = "none";
+    moreDescription.forEach((description) => {
+      description.style.display = "none";
+    });
+    document.querySelector(".container").classList.remove("no-scroll");
+    readMoreButton.textContent = "Wyświetl więcej informacji";
+  }
+}
+
+function navigation(sectionId) {
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: "smooth" });
+  const currentSection = document.querySelector(".section-expanded");
+
+  if (currentSection) {
+    const currentSectionId = currentSection.id;
+    toggleDescription(currentSectionId);
   }
 }
